81c7d850734c8bdaf087623dda95c26e23875a18
ak <alok@harmony.one>
2019-07-25 08:02:58 -0700

    modifications to node 2

--- scripts/node.sh
+++ scripts/node.sh
@@ -241,7 +241,7 @@ fi
 myip
 
 # public boot node multiaddress
-if [ !$testnet ]; then
+if [ "$testnet" == false ]; then
    BN_MA=/ip4/100.26.90.187/tcp/9874/p2p/Qmdfjtk6hPoyrH1zVD9PEH4zfWLo38dP2mDvvKXfh3tnEv,/ip4/54.213.43.194/tcp/9874/p2p/QmZJJx6AdaoEkGLrYG4JeLCKeCKDjnFz2wfHNHxAqFSGA9,/ip4/13.113.101.219/tcp/12019/p2p/QmQayinFSgMMw5cSpDUiD9pQ2WeP6WNmGxpZ6ou3mdVFJX,/ip4/99.81.170.167/tcp/12019/p2p/QmRVbTpEYup8dSaURZfF6ByrMTSKa4UyUzJhSjahFzRqNj
 else
    BN_MA=/ip4/54.213.43.194/tcp/9868/p2p/QmZJJx6AdaoEkGLrYG4JeLCKeCKDjnFz2wfHNHxAqFSGA9,/ip4/100.26.90.187/tcp/9868/p2p/Qmdfjtk6hPoyrH1zVD9PEH4zfWLo38dP2mDvvKXfh3tnEv,/ip4/13.113.101.219/tcp/12018/p2p/QmQayinFSgMMw5cSpDUiD9pQ2WeP6WNmGxpZ6ou3mdVFJX,/ip4/99.81.170.167/tcp/12018/p2p/QmRVbTpEYup8dSaURZfF6ByrMTSKa4UyUzJhSjahFzRqNj
@@ -375,7 +375,7 @@ do
    msg "############### Running Harmony Process ###############"
    if [ "$OS" == "Linux" ]; then
       # Run Harmony Node
-      if [ !$testnet ]; then
+      if [ "$testnet" == false ]; then
          if [ -z "${blspass}" ]; then
             echo -n "${passphrase}" | LD_LIBRARY_PATH=$(pwd) ./harmony -bootnodes $BN_MA -ip $PUB_IP -port $NODE_PORT -is_genesis -blskey_file "${BLSKEYFILE}" -blspass stdin
          else
@@ -389,7 +389,7 @@ do
          fi
       fi
    else
-      if [ !$testnet ]; then
+      if [ "$testnet" == false ]; then
          if [ -z "${blspass}" ]; then
             echo -n "${passphrase}" | DYLD_FALLBACK_LIBRARY_PATH=$(pwd) ./harmony -bootnodes $BN_MA -ip $PUB_IP -port $NODE_PORT -is_genesis -blskey_file "${BLSKEYFILE}" -blspass stdin 
          else
